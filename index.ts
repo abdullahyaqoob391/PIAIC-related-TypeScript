074ce6c6c06ae8ce60bbdd7983c767c9fa2c3e3a
abdullahyaqoob391 <140446519+abdullahyaqoob391@users.noreply.github.com>
2023-07-25 00:30:20 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,22 @@
+
+var a: number = 35;
+var b: number = 74;
+var c: number = 73;
+var d: number = 10;
+var e: number = 10;
+var f: number = 14;
+var adding:number = (a+b+c+d+e+f);
+console.log("By Adding Numbers");
+console.log(adding);
+
+var dividing:number = (a/b/c/d/e/f);
+console.log("By Dividing Numbers");
+console.log(dividing);
+
+var multiplying:number = (a*b*c*d*e*f);
+console.log("By Multiplying Numbers")
+console.log(multiplying);
+
+var subtracting:number = (a-b-c-d-e-f);
+console.log("By Subtracting Numbers");
+console.log(subtracting);
